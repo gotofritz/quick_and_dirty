f92799a5afcb65d07587ad8a36896b976a400ac5
fritz <you@example.com>
2018-04-04 02:13:08 +0200

    Convert in ffmpet can take a start and duration parameter

--- captain_mandolin/lib/ffmpeg.js
+++ captain_mandolin/lib/ffmpeg.js
@@ -1,21 +1,28 @@
 module.exports = {
   split: ({ src, start, duration, dest }) =>
     [
-      `ffmpeg -y`,
-      `-ss ${start}`,
-      duration ? `-t ${duration}` : '',
-      `-i "${src}"`,
-      '-vcodec copy -acodec copy',
+      `ffmpeg -y `,
+      `-ss ${start} `,
+      duration ? `-t ${duration} ` : '',
+      `-i "${src}" `,
+      '-vcodec copy -acodec copy ',
       `-sn "${dest}"`,
-    ].join(' '),
+    ].join(''),
 
-  convert: ({ src, dest }) =>
-    `ffmpeg -y -i "${src}" -c copy -bsf:v h264_mp4toannexb -f mpegts "${dest}"`,
+  convert: ({ src, start, duration, dest }) =>
+    [
+      `ffmpeg -y `,
+      start ? `-ss ${start} ` : '',
+      duration ? `-t ${duration} ` : '',
+      `-i "${src}" `,
+      ` -c copy -bsf:v h264_mp4toannexb `,
+      `-f mpegts "${dest}"`,
+    ].join(''),
 
   join: ({ src, dest }) =>
     [
-      `ffmpeg -i "concat:${src}"`,
-      '-c copy -bsf:a aac_adtstoasc',
+      `ffmpeg -i "concat:${src}" `,
+      '-c copy -bsf:a aac_adtstoasc ',
       `"${dest}"`,
-    ].join(' '),
+    ].join(''),
 };
