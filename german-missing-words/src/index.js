d638072667b2702c158032fec8940e0225d604b5
fritz <fritz@fritz.home>
2018-11-13 00:45:56 +0100

    WIP german-missin-words

--- german-missing-words/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/reset.css';
-import App from './app/App';
-import registerServiceWorker from './assets/registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
