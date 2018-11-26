d6062c7c1847410323e37f6a8c9bc65efc4c0a6d
fritz <fritz@fritz.home>
2018-11-26 01:21:10 +0100

    ANKI-PYTHON remove obsolete files

--- anki-python/anki.py
+++ anki-python/anki.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import argparse
+import markdown
+import re
 
+md = markdown.Markdown(output_format='html5')
+md_start = len('<p>')
+md_end = len('</p>')
 SEPARATOR = 2
-TAGS = 'geeky blockchain'
+DEFAULT_TAGS = 'geeky '
 
 parser = argparse.ArgumentParser(description='Creates importable anki files')
 parser.add_argument('--fields', dest='fields', required=True, type=int,
@@ -11,10 +16,15 @@ parser.add_argument('--src', dest='src', required=True,
                     help='Text file to scan')
 parser.add_argument('--dest', dest='dest', default='anki.txt',
                     help='Text file to write to')
+parser.add_argument('--tags', dest='tags', default='',
+                    help='Tags to add to defaut (default: geeky')
 args = parser.parse_args()
 
 def write_card_to_file(the_card, the_file):
-  the_file.write("\t".join(the_card) + "\n")
+  # don't write empty cards
+  should_write = any(bool(field) for field in the_card[:-1])
+  if should_write:
+    the_file.write("\t".join(the_card) + "\n")
 
 
 cards = []
@@ -23,7 +33,7 @@ trigger_new_card = SEPARATOR - 1
 total_cards = 0
 card_pointer = 0
 new_card_template = [''] * args.fields
-new_card_template.append(TAGS)
+new_card_template.append(DEFAULT_TAGS + args.tags)
 new_card = new_card_template.copy()
 
 write_to = open(args.dest, 'w', encoding='utf-8')
@@ -39,7 +49,12 @@ with  open(args.src) as read_from:
         card_pointer += 1
       newlines += 1
     else:
-      new_card[card_pointer] = line.strip()
+      line = line.strip()
+      if not re.match('<pre>', line):
+        # sadly markdown wraps everything in <p>...</p>
+        # we take the substring inside those
+        line = md.convert(line)[md_start:-md_end]
+      new_card[card_pointer] = line
       card_pointer += 1
       newlines = 0
 write_card_to_file(new_card, write_to)
