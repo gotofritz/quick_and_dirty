ef71ddbb489cddbcb868f610775eb287db6535ba
gotofritz <info@gotofritz.net>
2019-06-26 19:58:59 +0200

    Remove obsolete folder

--- german-missing-words-app/client/src/actions/actions.js
+++
@@ -1,6 +0,0 @@
-import { ADD_TEXT } from '../constants/action-types';
-
-export const addText = text => ({
-  type: ADD_TEXT,
-  payload: text,
-});
--- german-missing-words-app/client/src/constants/action-types.js
+++
@@ -1,1 +0,0 @@
-export const ADD_TEXT = 'ADD_TEXT';
