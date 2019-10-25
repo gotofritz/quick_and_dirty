5a3fdf1d23026ac4563f454c2a1c84fa0f131324
fritz <fritz@example.com>
2019-10-25 23:07:48 +0200

    feat(copy): add a simple script to move files around

---
+++ captain_mandolin/copy_files/sort.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+const glob = require('glob');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+
+const DEFAULT_DEST = path.resolve('./_RESORTED');
+const DEFAULT_DIRS = 47;
+const SUFFIX = '/**/*.mp4';
+const PAD = 2;
+
+let src = argv.s || argv.src;
+if (!src) {
+  throw Error('No -s or --src passed');
+}
+
+const max = argv.m || argv.max || DEFAULT_DIRS;
+const dest = argv.d || argv.dest || DEFAULT_DEST;
+
+src = path.resolve(argv.s || argv.src);
+
+const sortFn =
+  argv.r || argv.random ? () => (Math.random() > 0.5 ? 1 : -1) : () => -1;
+
+const sources = glob.sync(src + SUFFIX).sort(sortFn);
+
+const dt = max / sources.length;
+// start with a random int
+let offset = Number(
+  Math.random()
+    .toString()
+    .substr(2)
+);
+
+for (let i = 0; i < sources.length; i++) {
+  const prefix = String(1 + (Math.round(offset) % max)).padStart(PAD, '0');
+  const destPath = path.join(
+    dest,
+    prefix + ' ' + path.basename(sources[i]).replace(/^\d+ /, '')
+  );
+  offset += dt;
+  console.log(`COPYING ${sources[i]} ${destPath}`);
+  fs.copyFileSync(sources[i], destPath);
+}
+
+console.log('DONE');
