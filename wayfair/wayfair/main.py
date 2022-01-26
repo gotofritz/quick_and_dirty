4f91ea79e756ef144ac8ded4863bf5048c3a73e6
gotofritz <gotofritz@users.noreply.github.com>
2022-01-26 13:53:11 +0100

    Add work related scripts

---
+++ wayfair/wayfair/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv(r"/Users/fs740y/Downloads/org_browser_direct_reports_1003175.csv")
+column_list = df.columns.tolist()
+del column_list[4], column_list[0]
+df = df[column_list]
+for num, row in df.iterrows():
+    print(f"{row[0]} {row[1]} - {row[2]}")
