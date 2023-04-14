f7e8475b3865433d3d8c313cadf0000b6eb66723
gotofritz <gotofritz@users.noreply.github.com>
2023-04-14 16:02:09 +0200

    add discarded and duplicates checks

--- anki-python/anki/urls.py
+++ anki-python/anki/urls.py
@@ -26,22 +26,34 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+discarded = 0
+duplicates_ignored = 0
+duplicates = []
+
 
 def handle_urls(record):
+    global discarded
+    global duplicates_ignored
+    global duplicates
     urls = []
     last_book = ""
     last_tags = ""
     for src in record["src"]:
+        if src in duplicates:
+            duplicates_ignored += 1
+            continue
         webbrowser.open(src, new=2)
         keep = input("Keep? [Y/n]")
         book = ""
         tags = ""
-        if keep.lower() != "n":
+        if keep.lower() == "n":
+            discarded += 1
+        else:
             old_book = record["book"] if "book" in record else ""
             while not book:
                 book = input(
                     f"Book? [{old_book or last_book}] or "
-                    + "'might', 'explore', 'tools', 'idea', 'katas', 'knowledge', 'gallery'\n"
+                    + "'might', 'explore', 'tools', 'idea', 'interesting', 'katas', 'knowledge', 'gallery'\n"
                 )
                 if not book:
                     book = old_book or last_book
@@ -69,16 +81,29 @@ def handle_urls(record):
 
 
 def main():
+    global discarded
+    global duplicates
+    global duplicates_ignored
     how_many_records_todo = 3
     print(f"Called with: src: {args.src}, dest: {args.dest}, todo: {args.todo}")
     urls = yaml.load(open(args.dest), Loader=yaml.FullLoader)
+    duplicates = {url for record in urls for url in record["src"]}
 
     all_records = yaml.load(open(args.src), Loader=yaml.FullLoader)
 
     records_todo = all_records[0:how_many_records_todo]
     just_copy = all_records[how_many_records_todo:]
 
+    MAX_LINKS = 5
     for record in records_todo:
+        if len(record["src"]) > MAX_LINKS:
+            new_record = {
+                "book": record.get("book", ""),
+                "tags": record.get("tags"),
+                "src": record["src"][MAX_LINKS:],
+            }
+            just_copy.append(new_record)
+            del record["src"][MAX_LINKS:]
         urls = urls + handle_urls(record)
 
     with open(args.dest, "w") as f:
@@ -87,7 +112,13 @@ def main():
     with open(args.todo, "w") as f:
         f.write(yaml.dump(just_copy))
 
-    print(f"Done\n\tcleaned up file {args.dest}\n\tstill to do {args.todo}")
+    print(
+        "Done"
+        + f"\n\t{len(urls)} cleaned up files in {args.dest}"
+        + f"\n\t{discarded} discarded"
+        + f"\n\t{duplicates_ignored} duplicates"
+        + f"\n\t{len(just_copy)} still to do in {args.todo}"
+    )
 
 
 if __name__ == "__main__":
