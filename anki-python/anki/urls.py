39e8e0c8697e6d36dcc9439d20089b821ef0498e
gotofritz <gotofritz@users.noreply.github.com>
2023-05-20 00:34:34 +0200

    small clean up

--- anki-python/anki/urls.py
+++ anki-python/anki/urls.py
@@ -2,6 +2,15 @@ import yaml
 import argparse
 from pathlib import Path
 import webbrowser
+import re
+import subprocess
+
+
+def write_to_clipboard(output):
+    process = subprocess.Popen(
+        "pbcopy", env={"LANG": "en_GB.UTF-8"}, stdin=subprocess.PIPE
+    )
+    process.communicate(output.encode("utf-8"))
 
 
 parser = argparse.ArgumentParser(description="Updates URLs yaml file")
@@ -33,20 +42,38 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+
+run = True
+
 discarded = 0
 duplicates_ignored = 0
 duplicates = []
 
 
+def handle_input(prompt: str):
+    """Wrap input with the ability to quite gracefully."""
+    global run
+
+    QUIT = "---"
+    user_text = input(prompt)
+    if user_text == QUIT:
+        run = False
+        return None
+    return user_text
+
+
 def handle_urls(record):
     def get_book(old_book):
         nonlocal last_book
+        global run
         book = ""
         while not book:
-            book = input(
+            book = handle_input(
                 f"Book? [{old_book or last_book}] or "
                 + "'might', 'explore', 'tools', 'idea', 'interesting', 'katas', 'knowledge', 'gallery'\n"
             )
+            if not run:
+                break
             if not book:
                 book = old_book or last_book
             if book:
@@ -57,7 +84,9 @@ def handle_urls(record):
         nonlocal last_tags
         tags = ""
         while not tags:
-            tags = input(f"Tags? [{old_tags or last_tags}]\n")
+            tags = handle_input(f"Tags? [{old_tags or last_tags}]\n")
+            if not run:
+                break
             if not tags:
                 tags = old_tags or last_tags
             if tags:
@@ -67,16 +96,24 @@ def handle_urls(record):
     global discarded
     global duplicates_ignored
     global duplicates
+    global run
+
     urls = []
     last_book = ""
     last_tags = ""
     for src in record["src"]:
+        if not run:
+            break
         if src in duplicates:
             duplicates_ignored += 1
             continue
-        print(f"BOOK: {record['book']} / TAGS: {record['tags']}")
-        webbrowser.open(src, new=2)
-        keep = input("Keep? [Y/n]")
+        display_tags = re.sub(r"[, ]+", " ", record["tags"])
+        print(f"{record['i']}] BOOK: {record['book']}\t\nTAGS: {display_tags}")
+        write_to_clipboard(display_tags)
+        webbrowser.open(src, new=2, autoraise=False)
+        keep = handle_input("Keep? [Y/n]")
+        if not run:
+            break
         if keep.lower() == "n":
             discarded += 1
         else:
@@ -102,6 +139,8 @@ def main():
     global discarded
     global duplicates
     global duplicates_ignored
+    global run
+
     how_many_records_todo = args.n
     print(
         f"Called with: src: {args.src}, dest: {args.dest}, todo: {args.todo}, number: {args.n}"
@@ -115,7 +154,10 @@ def main():
     just_copy = all_records[how_many_records_todo:]
 
     MAX_LINKS = 8
-    for record in records_todo:
+
+    i = 0
+    while len(records_todo):
+        record = records_todo.pop(0)
         if isinstance(record["src"], str):
             record["src"] = [record["src"]]
         if len(record["src"]) > MAX_LINKS:
@@ -129,9 +171,13 @@ def main():
 
         urls = [
             url
-            for url in handle_urls(record)
+            for url in handle_urls({"i": i} | record)
             if url["book"] != "delete" and url["tags"] != "delete"
         ] + urls
+        if not run:
+            just_copy = [record] + records_todo + just_copy
+            break
+        i += 1
 
     with open(args.dest, "w") as f:
         f.write(yaml.dump(urls))
